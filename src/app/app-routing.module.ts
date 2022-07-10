8ecfd000947cf3f6fcefd49c05a132b0dfe5c579
RIA GEORGE <riageorge987@gmail.com>
2022-07-11 04:59:59 +0530

    portfolio

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
+import { HomeComponent } from './home/home.component';
+import { ReferenceComponent } from './reference/reference.component';
+import { ServicesComponent } from './services/services.component';
+import { SkillsComponent } from './skills/skills.component';
+
+
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"education",component:EducationComponent},
+  {path:"skills",component:SkillsComponent},
+  {path:"reference",component:ReferenceComponent},
+  {path:"services",component:ServicesComponent}
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
